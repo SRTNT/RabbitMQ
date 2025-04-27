0249a05a21821b5dd7594511d5ab014d8451c741
SRT Mokaab <s.r.taheri@gmail.com>
2025-04-27 12:56:16 +0330

    Update

---
+++ src/ReciverAPP1/Receive.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using ReciverAPP1.Controllers;
+
+namespace ReciverAPP1
+{
+    public class Receive
+    {
+        private readonly ILogger<Receive> _logger;
+
+        public Receive(ILogger<Receive> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task ReadyForGet()
+        {
+            bool isFinished = false;
+
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "sa",
+                Password = "SaeedTNT220",
+                VirtualHost = "/"
+            };
+
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "task_queue",
+                                            durable: true, // For Not Delete if the rabbitmq crash => true
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            // Control how many message analyze by this consumer
+            await channel.BasicQosAsync(prefetchSize: 0, // Size of message in byte - 0 = no limit 
+                                        prefetchCount: 1, // number of message that send to consumer for analyze
+                                        global: false); // true => all connect consumer get this config
+                                                        // false => just this consumer has this config
+
+            _logger.LogInformation(" [*] Waiting for messages.");
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                _logger.LogWarning($" [x] Received {message}");
+
+                await Task.Delay(1000);
+
+                // here channel could also be accessed as ((AsyncEventingBasicConsumer)sender).Channel
+                await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+
+                isFinished = true;
+            };
+
+            await channel.BasicConsumeAsync(queue: "task_queue",
+                                            autoAck: false,
+                                            consumer: consumer);
+
+            while (!isFinished)
+            {
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
---
+++ src/SenderAPP/Sender.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace SenderAPP
+{
+    public class Sender
+    {
+        private readonly ILogger<Sender> _logger;
+
+        public Sender(ILogger<Sender> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task SendData(string message)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "sa",
+                Password = "SaeedTNT220",
+                VirtualHost = "/"
+            };
+
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "task_queue",
+                                            durable: true, // For Not Delete if the rabbitmq crash => true
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+            var properties = new BasicProperties
+            {
+                Persistent = true // if the server has crash:
+                                  // true:  keep message for next run
+                                  // false: delete message
+            };
+
+            await channel.BasicPublishAsync(exchange: string.Empty,
+                                            routingKey: "task_queue",
+                                            mandatory: true, // if the rout key not existed return message to main server
+                                            basicProperties: properties,
+                                            body: body);
+
+            _logger.LogInformation($" [x] Sent {message}");
+        }
+    }
+}
